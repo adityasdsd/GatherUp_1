24d1b099cdb314959fc730584cd89627bab313d7
adityasdsd <adipatel719@gmail.com>
2025-02-01 12:13:38 +0530

    Added clg in main file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-console.log("HII");
+console.log("HIIIII");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
